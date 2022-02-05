90e23996dcd975ad380c3f394054fbae94d14aa5
MatheusFranzener <matheus_hohmann@estudante.sc.senai.br>
2022-02-04 21:28:38 -0300

    CommitModulos

--- EconomizaJaragua/economiza-jaragua/src/app/app.module.ts
+++ EconomizaJaragua/economiza-jaragua/src/app/app.module.ts
@@ -2,16 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { InformacoesMercadosComponent } from './mercado/informacoes-mercados/informacoes-mercados.component';
-import { PromocoesMercadosComponent } from './mercado/promocoes-mercados/promocoes-mercados.component';
-import { CadastrarMercadoComponent } from './mercado/cadastrar-mercado/cadastrar-mercado.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    InformacoesMercadosComponent,
-    PromocoesMercadosComponent,
-    CadastrarMercadoComponent
+
   ],
   imports: [
     BrowserModule
--- EconomizaJaragua/economiza-jaragua/src/app/mercados/mercados.module.ts
+++ EconomizaJaragua/economiza-jaragua/src/app/mercados/mercados.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { InformacoesMercadosComponent } from './informacoes-mercados/informacoes-mercados.component';
+import { PromocoesMercadosComponent } from './promocoes-mercados/promocoes-mercados.component';
+import { CadastrarMercadoComponent } from './cadastrar-mercado/cadastrar-mercado.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [InformacoesMercadosComponent, PromocoesMercadosComponent, CadastrarMercadoComponent]
 })
 export class MercadosModule { }
