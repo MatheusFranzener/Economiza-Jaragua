fcc289fc48dbd0e8e8c6883481cebc9d8f510735
FelipeMielkeVieira <92130158+MatheusFranzener@users.noreply.github.com>
2022-04-25 15:09:21 -0300

    cadastroMercado

--- EconomizaJaragua/economiza-jaragua/src/app/mercados/cadastrar-mercado/cadastrar-mercado.component.ts
+++ EconomizaJaragua/economiza-jaragua/src/app/mercados/cadastrar-mercado/cadastrar-mercado.component.ts
@@ -132,13 +132,17 @@ export class CadastrarMercadoComponent implements OnInit {
 
   cadastrarMercado(codigo) {
     var self = this;
-    fetch('http://localhost:3000/api/cadastrar_mercado', {
+    if(this.cnpj == ""||this.nome_mercado == ""||this.telefone == ""||this.email == ""){
+      alert("Preencha todos os dados!")
+    } else {
+      fetch('http://localhost:3000/api/cadastrar_mercado', {
       method: 'POST', body: JSON.stringify({
 
         cnpj: self.cnpj, nome_mercado: self.nome_mercado, telefone: self.telefone, email: self.email, codigo: codigo, logo64: self.logo64
       }), headers: { "Content-Type": "application/json" }
     });
     self.router.navigate(['home'])
+    }
   }
 
   mudanca(file) {
